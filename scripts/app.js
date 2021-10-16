a2ab07097e610842a7f12c289c0e3992ac6cf429
Zach <zbehan@gmail.com>
2021-10-16 11:41:36 +1100

    Create divide function

--- scripts/app.js
+++ scripts/app.js
@@ -9,3 +9,10 @@ function subract(a, b) {
 function multiply(a, b) {
     return a * b;
 }
+
+function divide(a, b) {
+    if (a === 0 || b === 0) {
+        return 'Unable to divide by zero';
+    }
+    return a / b;
+}
